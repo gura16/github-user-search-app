25441a1d0a2dcc66c925387d0258a1b2eb534998
guramikvakhadze <gkvaxadze@gmail.com>
2023-08-13 18:04:21 +0100

    done mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import sun from "./image/sun.png";
 import dark from "./image/dark.png";
 import search from "./image/search.png";
-
+import oval from "./image/Oval.png";
+import github from "./image/github.png";
+import twitter from "./image/twitter.png";
+import link from "./image/link.png";
+import location from "./image/location.png";
 function App() {
   return (
     <div className="body">
@@ -27,6 +31,46 @@ function App() {
             placeholder="Search GitHub username…"
           ></input>
           <button className="button">Search</button>
+        </div>
+        <div className="container3">
+          <div className="titlediv">
+            <img src={oval} />
+            <div className="titleinfo">
+              <h2>The Octocat</h2>
+              <p className="mail">@octocat</p>
+              <p className="date">Joined 25 Jan 2011</p>
+            </div>
+          </div>
+          <h3>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+            odio. Quisque volutpat mattis eros.
+          </h3>
+          <div className="followersdiv">
+            <p className="followingtext">
+              <span>Repos</span>
+              <span className="followers">Followers</span>
+              <span>Following</span>
+            </p>
+            <p className="followingnumber">
+              <span>8</span>
+              <span>3938</span>
+              <span>9</span>
+            </p>
+          </div>
+          <div className="contactdiv">
+            <div className="icons">
+              <img className="icon" src={location} />
+              <img className="icon" src={link} />
+              <img className="icon" src={twitter} />
+              <img className="icon" src={github} />
+            </div>
+            <div className="textdiv">
+              <p className="icontext">San Francisco</p>
+              <p className="icontext">https://github.blog</p>
+              <p className="icontext">Not Available</p>
+              <p className="icontext">@github</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
